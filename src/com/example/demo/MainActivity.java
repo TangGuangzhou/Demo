5587b7bb57554882c8a0d607829b9472c4f3a4c3
TangGuangzhou <tousama_orz@yeah.net>
2014-12-17 17:56:44 +0800

    测试1

--- src/com/example/demo/MainActivity.java
+++ src/com/example/demo/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +9,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        System.out.print("hehe");
     }
 }
