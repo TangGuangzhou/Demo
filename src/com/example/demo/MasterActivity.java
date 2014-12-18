b15e3c7ab07ca090709c9b83d810a4a7f1fe2b10
TangGuangzhou <tousama_orz@yeah.net>
2014-12-18 13:59:25 +0800

    Master

---
+++ src/com/example/demo/MasterActivity.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MasterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_master);
+		
+		System.out.print("I'm a master");
+	}
+}
