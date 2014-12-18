dbfa077fcccfe9448aff545025fea05157678dc7
TangGuangzhou <tousama_orz@yeah.net>
2014-12-18 14:00:51 +0800

    branch

--- src/com/example/demo/BranchActivity.java
+++ src/com/example/demo/BranchActivity.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class BranchActivity extends Activity {
 
